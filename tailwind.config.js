1ebcf0311517aa667fe751f1989cb17ae02bd942
Tejaswini2004313 <tejaswinipatkar55@gmail.com>
2024-10-15 12:46:36 +0530

    updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
-const flowbite = require("flowbite-react/tailwind");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    flowbite.content(),
+    "./node_modules/flowbite-react/**/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
       colors: {
-        'whitesmoke' : '#f5f5f5',
-        'bgcolor' : '#001C30',
-        'blue' : '#88E1FF',
-        'gray' : '#D9D9D9'
-      }
+        whitesmoke: "#f5f5f5",
+        bgcolor: "#001C30",
+        blue: "#88E1FF",
+        gray: "#D9D9D9",
+      },
     },
   },
-  plugins: [
-    flowbite.plugin(),
-  ],
-}
-
+  plugins: [require("flowbite/plugin")],
+};
